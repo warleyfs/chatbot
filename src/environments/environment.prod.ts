03d9da5864968ce3a2b3a512e210ee5c0267ec08
Warley Ferreira Silva <warley.ferreira@gmail.com>
2023-03-16 19:09:36 -0300

    Ajustes parciais na tela de chat

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   chatGPT: {
     endpoint: 'https://api.openai.com/v1/chat/completions',
-    token: ''
+    token: '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'
   }
 };
